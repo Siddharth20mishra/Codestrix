fa5d9d35784bcba92ea08f392d5b30e0aba5c5c2
siddharth20mishra <sm8725086@gmail.com>
2025-05-06 11:08:59 +0530

    register route is done

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+import bcrypt from "bcryptjs";
+import {db} from "../libs/db.js"
+import {UserRole} from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req , res)=>{
+    const {email , password , name}=req.body;
+//check user already exist or not
+    try {
+       const existingUser = await db.user.findUnique({
+        where:{
+            email
+        }
+       })
+       if(existingUser){
+        return res.status(400).json({
+            error:"user already exist"
+        })
+       }
+
+       const hashedPassword = await bcrypt.hash(password , 10);
+       //onboarding user in db
+       const newUser = await db.user.create({
+        data:{
+            email,
+            password:hashedPassword,
+            name,
+            role:UserRole.USER
+        }
+       })
+
+       const token =jwt.sign({id:newUser.id} , process.env.JWT_SECRET , {
+        expiresIn:"7d"
+       })
+
+       res.cookie("jwt",token , {
+        httpOnly:true,
+        sameSite:"strict",
+        secure:process.env.NODE_ENV !=="devlopment",
+        maxAge:1000*60*60*24*7   //7days
+       })
+
+       res.status(201).json({
+        success:true,
+        message:"User created successfully",
+        user:{
+            id:newUser.id,
+            email:newUser.email,
+            name:newUser.name,
+            role:newUser.role,
+            image:newUser.image
+        }
+    })
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({
+            error:"Error creating user"
+        }) 
+    } 
+}
+
+export const login = async (req , res)=>{}
+
+export const logout = async (req , res)=>{}
+
+export const check = async (req , res)=>{}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json()); //accpet the data in json
+app.use(cookieParser());
+//normal home route
+app.get("/" , (req,res)=>{
+    res.send("Hello guys welcome to codestrix☠️")
+})
+
+
+app.use("/api/v1/auth" , authRoutes );
 
 app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 8080");
