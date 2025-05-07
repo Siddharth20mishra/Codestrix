b9ebf82c92abcc17ba8c976e4f8b2d0027bf6f30
siddharth20mishra <sm8725086@gmail.com>
2025-05-07 16:07:41 +0530

    added boiler plates for routes and controllers for problems

---
+++ Backend/src/controllers/problem.controller.js
@@ -0,0 +1,11 @@
+export const createProblem=async(requestAnimationFrame,res)=>{}
+
+export const getAllProblems=async(requestAnimationFrame,res)=>{}
+
+export const getProblemById=async(requestAnimationFrame,res)=>{}
+
+export const updateProblem=async(requestAnimationFrame,res)=>{}
+
+export const deleteProblem=async(requestAnimationFrame,res)=>{}
+
+export const getAllProblemsSolvedByUser=async(requestAnimationFrame,res)=>{}
