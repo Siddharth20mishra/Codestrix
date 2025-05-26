ad8d594a0d396191c4303ff096325789659f6d63
siddharth20mishra <sm8725086@gmail.com>
2025-05-27 02:38:07 +0530

    frontend setup done

---
+++ Frontend/src/page/SignUpPage.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Link } from 'react-router-dom' 
+import {
+  Code,
+  Eye,
+  EyeOff,
+  Loader2,
+  Lock,
+  Mail,
+} from "lucide-react";
+const SignUpPage = () => {
+  return (
+    <div>
+      SignUpPage
+    </div>
+  )
+}
+
+export default SignUpPage
