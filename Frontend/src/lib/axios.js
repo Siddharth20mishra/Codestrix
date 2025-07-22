26f929caf6d8703c3e482edfc2d85d1eba5a1aa6
siddharth20mishra <sm8725086@gmail.com>
2025-07-22 19:07:50 +0530

    fix axios

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
-import axios from "axios";
+ import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+//   withCredentials: true,
+// });
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:8080/api/v1"
+      : "https://codestrix.onrender.com/api/v1",
   withCredentials: true,
 });
